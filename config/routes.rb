a94b9cdc833fc8ba6659f22ad2296d55ebfa0e93
Abdul <abdulbasitm929@gmail.com>
2016-06-06 22:38:08 +0000

    final

--- config/routes.rb
+++ config/routes.rb
@@ -6,17 +6,29 @@
 # We make no guarantees that this code is fit for any purpose.
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
-Rails.application.routes.draw do
+Depot::Application.routes.draw do
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get  'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+  get "sessions/create"
+  get "sessions/destroy"
+  resources :users
+  resources :orders
   resources :line_items
   resources :carts
-  get 'store/index'
 
-  resources :products
+  get "store/index"
+  resources :products do
+    get :who_bought, on: :member
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
   # You can have the root of your site routed with "root"
   root 'store#index', as: 'store'
   # ...
@@ -55,7 +67,7 @@ Rails.application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-
+  
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
